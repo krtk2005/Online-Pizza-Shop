c9f3c00c2ac5f96440f13f732095e9e151297fe4
Karthick <124263242+krtk2005@users.noreply.github.com>
2025-08-17 23:49:28 +0530

    Add files via upload

---
+++ partials/_footer.php
@@ -0,0 +1,3 @@
+<div class="footer container-fluid bg-dark text-light">
+    <p class="text-center py-2 mb-0">Copyright © 2025 Designed by <a href="https://github.com/krtk2005" target="_blank" rel="noopener noreferrer">@krtk2005</a></p>
+</div>
